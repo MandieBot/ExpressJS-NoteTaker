f015af8248b3d02878be860edf06c461827d715a
MandieBot <amandabotinelly@gmail.com>
2022-11-29 08:05:57 -0700

    Writing and testing routes to API

---
+++ routes/apiroutes.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const router = require("express").Router();
+const fs = require("fs");
+
+router.get("/notes", (req, res) => {
+  console.log("HELLO");
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const jsonData = JSON.parse(data);
+    console.log(jsonData);
+    res.json(jsonData);
+  });
+});
+
+router.post("/notes", (req, res) => {
+  console.log(req.body);
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const jsonData = JSON.parse(data);
+    jsonData.push(req.body);
+    console.log(jsonData);
+  });
+  fs.writeFile("./db/db.json", JSON.stringify(jsonData), (err) => {
+    if (err) console.log(err);
+    // else {
+    //   console.log("File written successfully\n");
+    //   console.log("The written has the following contents:");
+    // }
+  });
+  // read from the file here and then save it to a variable
+  //after it's read you need to return it to the user
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const htmlRoutes = require("./routes/htmlroutes");
+const apiRoutes = require("./routes/apiroutes");
 const app = express();
 const PORT = 3001;
-const path = require("path");
+// const path = require("path");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
 app.listen(PORT, () => console.log(`Serving static asset routes on port ${PORT}!`));
